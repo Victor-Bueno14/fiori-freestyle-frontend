15480b2f88d65ffc3899f7f00e979c41d27cc2bd
Victor Bueno <victorarrobabueno@gmail.com>
2025-09-02 14:57:49 -0300

    Manipulando Controllers

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,15 +1,31 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("zov.controller.View1", {
+        //Métodos do framework[->]
         onInit() {
         },
-        onPress(event) {
+        //Métodos do framework[<-]
+        onCalcular() {
+            const oView = this.getView();
+            const fB1 = parseFloat(oView.byId("b1").getValue());
+            const fB2 = parseFloat(oView.byId("b2").getValue());
+            const fB3 = parseFloat(oView.byId("b3").getValue());
+            const fB4 = parseFloat(oView.byId("b4").getValue());
+            let fResultado = 0;
 
-            alert("O " + event.mParameters.id + " foi pressionado!");
+            fResultado = (fB1 + fB2 + fB3 + fB4) / 4;
 
+            oView.byId("resultado").setValue(fResultado);
+
+            //Sem declarar a depedência.
+            //sap.m.MessageToast.show("Resultado: " + fResultado);
+
+            //Declarando a depedência. 
+            MessageToast.show("Resultado: " + fResultado);
         }
     });
 });
