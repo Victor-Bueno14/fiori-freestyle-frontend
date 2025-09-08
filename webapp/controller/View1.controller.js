9d4313b0aef29ef7669affeeee07778235d7d982
Victor Bueno <victorarrobabueno@gmail.com>
2025-09-08 16:02:31 -0300

    Manipulando Rotas

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -7,56 +7,22 @@ sap.ui.define([
     return Controller.extend("zov.controller.View1", {
         //Métodos do framework[->]
         onInit() {
-
-            let oModel1 = new sap.ui.model.json.JSONModel();
-            let oModel2 = new sap.ui.model.json.JSONModel();
-            let oModel3 = new sap.ui.model.json.JSONModel();
-
-            oModel1.setData({"usuario": {"nome": "Victor"}});
-            oModel1.setDefaultBindingMode(sap.ui.model.BindingMode.OneWay);
-            this.getView().setModel(oModel1,"oneway");
-
-            oModel2.setData({"usuario": {"nome": "Mavih"}});
-            oModel2.setDefaultBindingMode(sap.ui.model.BindingMode.TwoWay);
-            this.getView().setModel(oModel2,"twoway");
-            
-            oModel3.setData({"usuario": {"nome": "Messi"}});
-            oModel3.setDefaultBindingMode(sap.ui.model.BindingMode.OneTime);
-            this.getView().setModel(oModel3,"onetime");
-
         },
         //Métodos do framework[<-]
 
-        onTestOneWay() {
+        onNewCustomer() {
 
-            let oModel = this.getView().getModel("oneway");
-            let oData = oModel.getData();
+            let route = sap.ui.core.UIComponent.getRouterFor(this);
 
-            oData.usuario.nome = "Victor Bueno";
-
-            oModel.setData(oData);
+            route.navTo("RouteCustomerNew");
 
         },
 
-        onTestTwoWay() {
+        onEditCustomer() {
 
-            let oModel = this.getView().getModel("twoway");
-            let oData = oModel.getData();
+            let route = sap.ui.core.UIComponent.getRouterFor(this);
 
-            oData.usuario.nome = "Mavih Dutra";
-
-            oModel.setData(oData);
-
-        },
-
-        onTestOneTime() {
-
-            let oModel = this.getView().getModel("onetime");
-            let oData = oModel.getData();
-
-            oData.usuario.nome = "Lionel Messi";
-
-            oModel.setData(oData);
+            route.navTo("RouteCustomerEdit",{CustomerId:1});
 
         }
     });
