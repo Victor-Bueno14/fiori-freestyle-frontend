edc96b3df68a5039534cf70e18324addf8bb9cd6
Victor Bueno <victorarrobabueno@gmail.com>
2025-09-25 11:38:31 -0300

    Adição de Tabela de Item e Outras Correções

--- webapp/Component.js
+++ webapp/Component.js
@@ -21,10 +21,6 @@ sap.ui.define([
 
             // enable routing
             this.getRouter().initialize();
-
-        sap.ui.require(["sap/ui/dom/includeStyleSheet"], function (includeStyleSheet) {
-            includeStyleSheet(sap.ui.require.toUrl("zov/css/style.css")); 
-        });
         }
     });
 });
